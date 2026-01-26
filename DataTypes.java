a203979b91a5451a79c8598d6cf0f6f0a5762216
Valluri Udaya Sri <udayasrivalluri354@gmail.com>
2026-01-26 21:23:46 +0530

    datatypes

--- DataTypes.java
+++ DataTypes.java
@@ -1,6 +1,26 @@
 public class DataTypes{
     public static void main(String[] args){
-        System.out.println("Hello World ");
+        System.out.println("Data Types In Java ");
+        System.out.println(2+3);
+        int int1 = 3;
+        int int2 = 4;
+        int int3 = int1+int2;
+        System.out.println("the sum is: " + int3);
+        byte by = 125;
+        short sh = 558;
+        long  l = 5856l; //long need to put l at last to make sure saying its long
+        float f = 5.8f; // here 5.8 is double but when you want to mention it as float you have mention f in the last 
+        double d = 5.8;
+        char c = 'k';
+        boolean b = true;
+        
+        System.out.println("byte value is: " + by);
+        System.out.println("short value is: " + sh);
+        System.out.println("long value is: " + l);
+        System.out.println("float value is: " + f);
+        System.out.println("double value is: " + d);
+        System.out.println("char value is: " + c);
+        System.out.println("boolean value is: " + b);
 
     }
 }
