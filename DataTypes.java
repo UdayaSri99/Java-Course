17cdd37819461c79b64d58b70f4d8f38526fd4e0
Valluri Udaya Sri <udayasrivalluri354@gmail.com>
2026-01-26 21:17:52 +0530

    1st Program

---
+++ DataTypes.java
@@ -0,0 +1,21 @@
+public class DataTypes{
+    public static void main(String[] args){
+        System.out.println("Hello World ");
+
+    }
+}
+
+
+//Primitive: integer, float, characters , boolean
+//integer : int-4bytes, byte=1 byte , short-2 bytes, Long - 8bytes
+//char : 2 bytes for char we have to use only single quots ''
+//Boolean in java Is only True or false not 0 or 1 like others
+//float : 4 bytes
+//double : 8 bytes
+
+
+//Non primitive : String , Arrays , Classes , Interfaces
+// String is not a primitive data type it is a class , a string is a sequence of characters
+//Arrays is a collection of similar data types
+//Classes are blueprints for creating objects
+//Interfaces are contracts that classes can implement
