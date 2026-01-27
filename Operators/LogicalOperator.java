52430cd470cead6b88ab439d1ae381d1c67251e7
Valluri Udaya Sri <udayasrivalluri354@gmail.com>
2026-01-27 16:11:02 +0530

    operators

---
+++ Operators/LogicalOperator.java
@@ -0,0 +1,22 @@
+//Logical Operators :
+// Logical operators are used to perform logical operations on boolean values.
+// There are three main logical operators in Java:
+// 1. Logical AND (&&): Returns true if both operands are true.
+// 2. Logical OR (||): Returns true if at least one operand is true.
+// 3. Logical NOT (!): Returns true if the operand is false (inverts the value).
+
+public class LogicalOperator{
+    public static void main(String[] args){
+
+        boolean a = true;
+        boolean b = false;
+        // Logical AND
+        System.out.println("a && b" + (a && a)); // false
+        // Logical OR
+        System.out.println("a || b " + (a || b)); // true
+        // Logical NOT
+        System.out.println("!a " + (!a)); // false
+        System.out.println("!b " + (!b)); // true
+    }
+
+}
--- Operators/RelationalOperators.java
+++ Operators/RelationalOperators.java
@@ -1,1 +1,33 @@
-//Relational operators : relational operators are used to compare two values or expressions. They return a boolean value (true or false) based on the comparison.           
+//Relational operators : relational operators are used to compare two values or expressions. They return a boolean value (true or false) based on the comparison.  
+// There are six main relational operators in Java:
+// 1. Equal to (==): Returns true if both operands are equal.
+// 2. Not equal to (!=): Returns true if both operands are not equal.
+// 3. Greater than (>): Returns true if the left operand is greater than the right operand.
+// 4. Less than (<): Returns true if the left operand is less than the right operand.
+// 5. Greater than or equal to (>=): Returns true if the left operand is greater than or equal to the right operand.
+// 6. Less than or equal to (<=): Returns true if the left operand is less than or equal to the right operand. 
+
+public class RelationalOperators {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+
+        // Equal to
+        System.out.println("a == b: " + (a == b)); // false
+
+        // Not equal to
+        System.out.println("a != b: " + (a != b)); // true
+
+        // Greater than
+        System.out.println("a > b: " + (a > b)); // false
+
+        // Less than
+        System.out.println("a < b: " + (a < b)); // true
+
+        // Greater than or equal to
+        System.out.println("a >= b: " + (a >= b)); // false
+
+        // Less than or equal to
+        System.out.println("a <= b: " + (a <= b)); // true
+    }
+}
