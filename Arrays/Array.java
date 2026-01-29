6a6466ec9ce9db004c1c76ffda4e675fa8c320c0
Valluri Udaya Sri <udayasrivalluri354@gmail.com>
2026-01-29 22:43:36 +0530

    array

---
+++ Arrays/Array.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class Array {
+    public static void main(String[] args){
+        int[] arr = {10,20,30,40,50,60};
+        System.out.println("First Element:" + arr[0]);
+        for(int i =0; i<arr.length;i++){
+            System.out.println(arr[i]);
+
+
+        }
+    }
+    
+}
