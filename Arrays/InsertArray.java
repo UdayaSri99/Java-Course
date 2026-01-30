ff732f891dfc0e8fd54eecbe73f25a7fb131c234
Valluri Udaya Sri <udayasrivalluri354@gmail.com>
2026-01-30 12:42:43 +0530

    Arrays

---
+++ Arrays/InsertArray.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class InsertArray {
+    public static void main(String[] args){
+        String[] arr = new String[6];
+        arr[0] = "Ammu";
+        arr[1] = "Ammu1";
+        arr[2] = "Ammu2";
+        arr[3] = "Amm3";
+        arr[4] = "Ammu4";
+        arr[5] = "Ammu5";
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);                 
+        System.out.println(arr[5]);
+
+
+    }
+    
+}
