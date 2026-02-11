a2988e119e63db77da1015519a0731bb51a51387
Valluri Udaya Sri <udayasrivalluri354@gmail.com>
2026-02-11 14:26:47 +0530

    array

---
+++ Arrays/ArrayString.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class ArrayString {
+    public static void main(String[] args){
+        String[] arr = {"Hello" , "World" , "Java" , "Programming"};
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+    
+}
